c777746ff0ff6fe9c905c618b3b1e0e08f6f31a4
Nubunduram <ceantoine@hotmail.fr>
2024-05-27 17:14:27 +0200

    Prototype with API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,69 @@
 <script setup>
-</script>
+import { ref } from 'vue';
 
+// Référence pour le bâtiment sélectionné
+const selectedBuilding = ref("");
+// Données des bâtiments et des points de contrôle des pièces
+const buildings = ref([]);
+const roomControlEndpoints = ref([]);
+
+// Fonction pour obtenir les étages pour le bâtiment sélectionné
+const getFloors = (buildingName) => {
+  const building = buildings.value.find(b => b.name === buildingName);
+  return building ? building.children : [];
+};
+let occuped;
+// Appel d'API pour récupérer les données des bâtiments
+fetch("https://api-developers.spinalcom.com/api/v1/geographicContext/space")
+  .then(response => response.json())
+  .then(data => {
+
+    buildings.value = data.children;
+    // Extracting dynamic ID from the response
+    const dynamicId = data.children[0].children[0].children[2].dynamicId;
+
+    // Use the dynamic ID to fetch room control endpoints
+    fetch(`https://api-developers.spinalcom.com/api/v1/room/${dynamicId}/control_endpoint_list`)
+      .then(response => response.json())
+      .then(data => {
+        roomControlEndpoints.value = data;
+        occuped = roomControlEndpoints.value[0].endpoints[4].currentValue;
+        // (If occuped return true then affiché Occupé, if it returns false, Non Occupé, if undefined/null return Undefined)
+        console.log(roomControlEndpoints.value[0].endpoints[4].currentValue)
+      })
+      .catch(error => {
+        console.error("Erreur lors de la récupération des données des points de contrôle des pièces :", error);
+      });
+  })
+  .catch(error => {
+    console.error("Erreur lors de la récupération des données des bâtiments :", error);
+  });
+
+</script>
 <template>
   <main>
-    <select>
-      <option value="">--Séléction du bâtiment--</option>
-      <option>Usine</option>
+    <select v-model="selectedBuilding">
+      <option value="">--Sélection du bâtiment--</option>
+      <option v-for="building in buildings" :key="building.staticId" :value="building.name">{{ building.name }}</option>
     </select>
-    <div>[Nom Etage 1] + [%Occupation]</div>
-    <div>[Nom Etage 2] + [%Occupation]</div>
-    <div>[Nom Etage 3] + [%Occupation]</div>
+
+    <details v-if="selectedBuilding">
+      <summary>{{ selectedBuilding }}</summary>
+      <ul>
+        <li v-for="floor in getFloors(selectedBuilding)" :key="floor.staticId">
+          <details>
+            <summary>{{ floor.name }}</summary>
+            <ul>
+              <li v-for="room in floor.children" :key="room.staticId">
+                <span>{{ room.name }}</span>
+                <span>{{ occuped }}</span>
+              </li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </details>
   </main>
 </template>
 
-<style lang="scss" scoped>
-main {
-  display: grid;
-  place-content: center;
-  max-width: 1280px;
-  margin: auto;
-}
-</style>
+<style lang="scss" scoped></style>

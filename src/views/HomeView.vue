92f1c0436bfc760bb9f086de3352e45016f40845
Nubunduram <ceantoine@hotmail.fr>
2024-05-27 15:07:20 +0200

    Initial Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <select>
+      <option value="">--Séléction du bâtiment--</option>
+      <option>Usine</option>
+    </select>
+    <div>[Nom Etage 1] + [%Occupation]</div>
+    <div>[Nom Etage 2] + [%Occupation]</div>
+    <div>[Nom Etage 3] + [%Occupation]</div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: grid;
+  place-content: center;
+  max-width: 1280px;
+  margin: auto;
+}
+</style>
